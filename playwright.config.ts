b3803e362838140ba4b4c844d9314325712a3c06
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-02-02 14:24:21 +0100

    chore: latest state

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
 
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "list",
+  workers: process.env.CI ? 1 : 1,
+  reporter: "html",
   use: {
     baseURL: "http://localhost:3000",
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     headless: false,
   },
   projects: [
---
+++ playwright/tests/sample.spec.ts
@@ -0,0 +1,50 @@
+import { HomePage } from "@/test/pages/home.page";
+import { NewTodoPage } from "@/test/pages/newTodo.page";
+import { UpdateTodoPage } from "@/test/pages/updateTodo.page";
+import { test, expect } from "@playwright/test";
+
+test("create Todo", async ({ page }) => {
+  const homePage = new HomePage(page);
+  const newTodoPage = new NewTodoPage(page);
+
+  await page.goto("/");
+  await homePage.authComponent.login("test@test.nl", "test");
+  const createdTodo = await newTodoPage.createNewTodo("test1234", "Important");
+  console.log(createdTodo);
+  await expect(
+    await homePage.todoContainer.todoItemComponent.getTodo(createdTodo.id)
+  ).toHaveText("test1234");
+});
+
+test("update Todo", async ({ page }) => {
+  const homePage = new HomePage(page);
+  const newTodoPage = new NewTodoPage(page);
+  const updateTodoPage = new UpdateTodoPage(page);
+
+  await page.goto("/");
+  await homePage.authComponent.login("test@test.nl", "test");
+  const createdTodo = await newTodoPage.createNewTodo("test57890", "Important");
+  console.log(createdTodo);
+  await homePage.todoContainer.todoItemComponent.editTodo(createdTodo.id);
+  await updateTodoPage.updateTodoDetails(
+    createdTodo.id,
+    "Ghislain",
+    "Not Important"
+  );
+  await expect(
+    await homePage.todoContainer.todoItemComponent.getTodo(createdTodo.id)
+  ).toHaveText("Ghislain");
+});
+
+test("delete Todo", async ({ page }) => {
+  const homePage = new HomePage(page);
+  const newTodoPage = new NewTodoPage(page);
+
+  await page.goto("/");
+  await homePage.authComponent.login("test@test.nl", "test");
+  const createdTodo = await newTodoPage.createNewTodo("test57890", "Important");
+  await homePage.todoContainer.todoItemComponent.deleteTodo(createdTodo.id);
+  await expect(
+    await homePage.todoContainer.todoItemComponent.getTodo(createdTodo.id)
+  ).not.toBeVisible();
+});
