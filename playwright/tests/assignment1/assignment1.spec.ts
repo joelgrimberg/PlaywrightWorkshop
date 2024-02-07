37a1fef6f0237ea62c9f6217095bb03d3b588d6b
Ghislain Gabrielse <Ghislain@Kadena.io>
2024-02-07 19:34:23 +0100

    add dumy test

---
+++ playwright/tests/assignment1/assignment1.spec.ts
@@ -0,0 +1,8 @@
+import { Generator } from "../../support/helpers/generator";
+import { test } from "../../support/fixtures/test.fixture";
+import { expect } from "@playwright/test";
+import exp from "constants";
+
+test("Assignment 1A", async ({ page }) => {
+  expect(true).toBeTruthy();
+});
